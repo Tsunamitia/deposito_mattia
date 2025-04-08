af040c8f78452e3eacea8e6780d5f1535f4ab5d4
Tsunamitia <mattia.mala.mm@gmail.com>
2025-04-08 12:17:50 +0200

    Update and rename esercizio4.py to esercizio0804.py

--- esercizio0804.py
+++ esercizio0804.py
@@ -1,3 +1,37 @@
+#controllo se il numero è pari o dispari esercizio 1
+numero=(int(input("inserisci un numero: "))) #input dell'utente
+#controllo se il numero è pari o dispari
+if numero%2==0:
+    #se il numero è pari
+    print("il numero è pari")
+#se il numero è dispari
+else:
+    print("il numero è dispari")
+
+#esercizio 2
+x=True
+while x:
+    #input dell'utente
+    numero=(int(input("inserisci un numero: ")))
+    #conto alla rovescia:
+    for i in range(numero,-1,-1):
+        print(i)
+
+    #chiedo all'utente se vuole continuare
+    risposta=input("vuoi continuare? (si/no)").lower()
+#controllo se l'utente vuole continuare
+    if risposta=="no":
+        #se l'utente non vuole continuare
+        x=False
+
+
+
+
+
+
+
+
+
 # Scrivi un programma che calcoli il quadrato di ogni numero in una lista di numeri.
 numeri=[2,3,4,4,5,6,7,8,9] #lista di numeri
 # Il programma calcola il quadrato di ogni numero nella lista e lo stampa.
